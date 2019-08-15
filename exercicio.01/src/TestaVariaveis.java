201c9694b2dca8a18c6e7e6c2e5a5bd0651c4aba
Luana <luanafernandes73@gmail.com>
2019-08-14 21:50:25 -0300

    Testa variaveis

---
+++ exercicio.01/src/TestaVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		String nome = "Luana";
+		System.out.println(nome);
+		
+		int idade = 30;
+		System.out.println(idade);
+		
+		idade += (7 * 5 + 2);
+		System.out.print(idade + "\n");
+		
+		System.out.println(nome + " tem " + idade + " anos.");
+		
+		double salario = 1270.50;
+		long valor = 57689659690766l;
+		float salario2 = 3.14f;
+		
+
+	}
+
+}
